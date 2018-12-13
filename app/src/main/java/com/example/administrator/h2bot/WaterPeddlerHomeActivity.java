da47e950103ac01993ce5227b592ca80bf0d49f2
MarcFamador <41323222+MarcFamador@users.noreply.github.com>
2018-12-14 00:17:55 +0800

    Added WaterPeddlerHomeActivity, no codes yet

---
+++ app/src/main/java/com/example/administrator/h2bot/WaterPeddlerHomeActivity.java
@@ -0,0 +1,13 @@
+package com.example.administrator.h2bot;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class WaterPeddlerHomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_water_peddler_home);
+    }
+}
