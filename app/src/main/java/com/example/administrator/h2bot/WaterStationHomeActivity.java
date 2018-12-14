7dab2ddba8ab6d3405e83c6a61b3db37428bdbf1
coroyoy1 <you@example.com>
2018-12-15 03:14:00 +0800

    Added Water Affiliate and Water Station Home Activity.

---
+++ app/src/main/java/com/example/administrator/h2bot/WaterStationHomeActivity.java
@@ -0,0 +1,13 @@
+package com.example.administrator.h2bot;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class WaterStationHomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_water_station_home);
+    }
+}
