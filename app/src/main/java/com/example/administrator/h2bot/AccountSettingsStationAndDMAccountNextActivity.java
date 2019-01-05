461e57861fa41182e069828f423fc7733b46e183
coroyoy1 <you@example.com>
2019-01-05 18:51:58 +0800

    Account Settings Water Station and DM, Update Document Water Station.

---
+++ app/src/main/java/com/example/administrator/h2bot/AccountSettingsStationAndDMAccountNextActivity.java
@@ -0,0 +1,13 @@
+package com.example.administrator.h2bot;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AccountSettingsStationAndDMAccountNextActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_account_settings_station_and_dmaccount_next);
+    }
+}
---
+++ app/src/main/java/com/example/administrator/h2bot/AccountSettingsStationAndDMActivity.java
@@ -0,0 +1,13 @@
+package com.example.administrator.h2bot;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AccountSettingsStationAndDMActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_account_settings_station_and_dm);
+    }
+}
