f2bd39ccb91a67b24489555f7e8e649f5419c4b9
MarcFamador <41323222+MarcFamador@users.noreply.github.com>
2019-01-06 21:27:53 +0800

    Water Peddler Documment Submittion UI Water Peddler Documment Submittion Update UI Update Account for Costumer, Affiliate, Water Peddler UI Update Account for Water Station, Delivery Guy UI Order Details UI Add item UI Update Item UI Daily Records UI

--- app/src/main/java/com/example/administrator/h2bot/WaterPeddlerHomeActivity.java
+++ app/src/main/java/com/example/administrator/h2bot/WaterPeddlerHomeActivity.java
@@ -13,20 +13,5 @@ public class WaterPeddlerHomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_water_peddler_home);
-
-        mDrawerLayout = (DrawerLayout)findViewById(R.id.drawer);
-        mToogle = new ActionBarDrawerToggle(this, mDrawerLayout,R.string.open,R.string.close);
-        mDrawerLayout.addDrawerListener(mToogle);
-        mToogle.syncState();
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        if(mToogle.onOptionsItemSelected(item))
-        {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
     }
 }
