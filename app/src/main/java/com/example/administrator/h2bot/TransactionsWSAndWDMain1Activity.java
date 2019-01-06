86d6dee6f95ae225fd7afb6f54a97505f7f0ab5d
coroyoy1 <you@example.com>
2019-01-06 16:00:41 +0800

    Product List Main 1, Transaction Main 1, and Map for Merchant(No code yet).

---
+++ app/src/main/java/com/example/administrator/h2bot/TransactionsWSAndWDMain1Activity.java
@@ -0,0 +1,13 @@
+package com.example.administrator.h2bot;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TransactionsWSAndWDMain1Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_transactions_wsand_wdmain1);
+    }
+}
