4d65c4068092f0de46892ff83ef745adf1c556d4
Ryan <imbanga69@gmail.com>
2019-01-08 22:02:28 +0800

    mga kuwang sa registration fixed

--- app/src/main/java/com/example/administrator/h2bot/RegisterActivity.java
+++ app/src/main/java/com/example/administrator/h2bot/RegisterActivity.java
@@ -2,14 +2,15 @@ package com.example.administrator.h2bot;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import java.util.ArrayList;
 
 public class RegisterActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
-
-
     }
 }
