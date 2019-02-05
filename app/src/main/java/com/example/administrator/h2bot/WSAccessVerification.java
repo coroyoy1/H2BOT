f7faf92ef225c74748c85a6c9a4f90fc50dce367
coroyoy1 <you@example.com>
2019-02-06 04:24:50 +0800

    WSVerfication and Other functionalities added.

--- app/src/main/java/com/example/administrator/h2bot/WSAccessVerification.java
+++ app/src/main/java/com/example/administrator/h2bot/WSAccessVerification.java
@@ -1,13 +1,42 @@
 package com.example.administrator.h2bot;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class WSAccessVerification extends AppCompatActivity {
+import com.google.firebase.auth.FirebaseAuth;
+
+public class WSAccessVerification extends AppCompatActivity implements View.OnClickListener {
+
+    Button buttonUpdate, buttonLogout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_wsaccess_verification);
+
+        buttonLogout = findViewById(R.id.logoutAV);
+        buttonUpdate = findViewById(R.id.updateAV);
+
+        buttonUpdate.setOnClickListener(this);
+        buttonLogout.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch(v.getId())
+        {
+            case R.id.logoutAV:
+                FirebaseAuth.getInstance().signOut();
+                finish();
+                startActivity(new Intent(WSAccessVerification.this, LoginActivity.class));
+                break;
+            case R.id.updateAV:
+                //Temp
+                startActivity(new Intent(WSAccessVerification.this, WaterStationDocumentVersion2Activity.class));
+                break;
+        }
     }
 }
