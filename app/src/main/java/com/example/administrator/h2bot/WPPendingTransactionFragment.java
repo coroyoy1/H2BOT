8902e8c8f01d9bb957bdd7a745fb75dfaafb6c5c
Ryan <imbanga69@gmail.com>
2019-02-11 21:13:09 +0800

    Transaction, Receipt of water Dealer (Makoy)

--- app/src/main/java/com/example/administrator/h2bot/WPPendingTransactionFragment.java
+++ app/src/main/java/com/example/administrator/h2bot/WPPendingTransactionFragment.java
@@ -78,6 +78,7 @@ public class WPPendingTransactionFragment extends Fragment {
             }
         });
         final Activity activity = getActivity();
+
         QRscanButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
