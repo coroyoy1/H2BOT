42f836cb3065a621a9924f82aa9426f96f822af4
coroyoy1 <you@example.com>
2019-02-16 07:38:16 +0800

    Delivery Man

--- app/src/main/java/com/example/administrator/h2bot/waterstation/WSDMFragment.java
+++ app/src/main/java/com/example/administrator/h2bot/waterstation/WSDMFragment.java
@@ -9,13 +9,20 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.example.administrator.h2bot.R;
 import com.example.administrator.h2bot.deliveryman.DMRegisterAccount;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class WSDMFragment extends Fragment implements View.OnClickListener{
 
     FloatingActionButton floatingActionButton;
+    FirebaseDatabase firebaseDatabase;
+    DatabaseReference databaseReference;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -40,6 +47,11 @@ public class WSDMFragment extends Fragment implements View.OnClickListener{
                         .replace(R.id.fragment_container_ws, additem)
                         .addToBackStack(null)
                         .commit();
+                Toast.makeText(getActivity(), FirebaseAuth.getInstance().getCurrentUser().getUid(), Toast.LENGTH_LONG).show();
+                String firebaseAuth  = FirebaseAuth.getInstance().getCurrentUser().getUid();
+                Bundle args = new Bundle();
+                args.putString("AuthenFirebase", firebaseAuth);
+                additem.setArguments(args);
                 break;
         }
     }
